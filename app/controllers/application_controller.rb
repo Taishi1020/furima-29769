d6c68b73a505c361478401f8fec16a067447f8ea
Taishi1020 <70305895+Taishi1020@users.noreply.github.com>
2020-09-24 19:54:34 +0900

    product comiito

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+ before_action :configure_permitted_parameters, if: :devise_controller?
+
+ protected
+ def configure_permitted_parameters
+   devise_parameter_sanitizer.permit(:sign_up, keys: [:birthday, :name_kana, :name_full_width_character, :nickname,:email])
+ end
 end
