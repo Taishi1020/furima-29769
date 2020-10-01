c33ef0f90ab8a2da0e2070d703c104819d44412d
Taishi1020 <70305895+Taishi1020@users.noreply.github.com>
2020-10-01 12:36:50 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root to: "furimas#index"
+  # get 'furimas/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :furimas do
   end
