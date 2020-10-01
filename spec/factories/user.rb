cae0793efeb85e80bce096c1e815fe6fc9668ccb
Taishi1020 <70305895+Taishi1020@users.noreply.github.com>
2020-10-01 10:52:43 +0900

    Revert "ユーザー管理機能 commit"
    This reverts commit e78e16e2e013fa68aeee320fb378aa0399f1e5b5.

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -4,10 +4,5 @@ FactoryBot.define do
     email                 {"kkk@gmail.com"}
     password              {"12345678"}
     password_confirmation {"12345678"}
-    first_name            {"山田"}
-    last_name             {"太郎"}
-    first_name_kana       {"ヤマダ"}
-    last_name_kana        {"タロウ"}
-    birthday              {"19991020"}
   end
 end
