d4ce782d82f193d41cb1cc986e343142abbd9df8
Taishi1020 <70305895+Taishi1020@users.noreply.github.com>
2020-10-01 15:51:06 +0900

    Revert "ユーザー管理機能とテストコード　commit"""""
    This reverts commit 2eb831d335c10012277c8120d677daa951e5c37a.

--- db/migrate/20200922075056_create_items.rb
+++ db/migrate/20200922075056_create_items.rb
@@ -1,15 +1,15 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-      t.string  :product_name,                null:false
-      t.integer :category_id,                 null:false
-      t.text    :product_description,         null:false
-      t.integer :ship_from_area_id,           null:false
-      t.integer :shipping_days_id,            null:false
-      t.integer :price,                       null:false
-      t.integer :product_condition_id,        null:false
-      t.integer :product_burden_id,           null:false
-      t.integer :user_id,                     null:false
+      t.integer :ship_from_area,      null: false
+      t.integer :shipping_days,       null: false
+      t.string  :product_name,        null: false
+      t.integer :category,            null: false
+      t.string  :method_of_payment,   null: false
+      t.integer :product_conditon,    null: false
+      t.integer :product_burden,      null: false
+      t.integer :price,               null: false
+      t.integer :days,                null: false
       t.timestamps
     end
   end
