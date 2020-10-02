92bcb98a2dafc549b9572406e8fa465fb3f7d9b7
Taishi1020 <70305895+Taishi1020@users.noreply.github.com>
2020-10-02 15:46:10 +0900

    validates commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,16 +9,15 @@ class User < ApplicationRecord
 
   
   with_options presence: true do
+    NAME_VALIDATES = /\A[ぁ-んァ-ン一-龥]+\z/
+    NAMEkana_VALIDATES = /\A[ァ-ヶー－]+\z/
     validates :nickname
     validates :birthday
-
-    
-  
     validates :password, format: { with: /\A[a-zA-Z0-9]+\z/,message: "Password Include both letters and numbers."}
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+/,message: "First name Full-width characters."}
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]+/,message: "Last name Full-width characters."}
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "First name kana full-width katakana characters."}
-    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "Last name kana last-width katakana characters."}
+    validates :first_name, format: { with: NAME_VALIDATES,message: "First name Full-width characters."}
+    validates :last_name, format: { with:  NAME_VALIDATES ,message: "Last name Full-width characters."}
+    validates :first_name_kana, format: { with: NAMEkana_VALIDATES, message: "First name kana full-width katakana characters."}
+    validates :last_name_kana, format: { with: NAMEkana_VALIDATES, message: "Last name kana last-width katakana characters."}
   end
 end
  
