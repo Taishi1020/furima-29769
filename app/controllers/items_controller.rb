e269ebc768e6c0a938fd6c64979f87e6bef0f71b
Taishi1020 <70305895+Taishi1020@users.noreply.github.com>
2020-10-16 00:20:51 +0900

    一覧機能2 commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,7 @@ class ItemsController < ApplicationController
   before_action :check_item_user, only: [:edit]
 
   def index
-    @items = Item.all
+    @items = Item.all.order{"imagae  ASC"}
   end
 
   def new
