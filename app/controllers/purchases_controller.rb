132c2ddfd53ea5014bf36995417f93be8d2a5721
Taishi1020 <70305895+Taishi1020@users.noreply.github.com>
2020-10-15 14:22:59 +0900

    購入機能4 commit

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -27,16 +27,19 @@ class PurchasesController < ApplicationController
 
   def set_item
     @item = Item.find(params[:item_id])
+  end
+
+  def donnation_params
+    params.require(:user_donation).permit(:name, :name_reading, :nickname, :postal_code, :prefecture_id, :city, :addresses, :building_name, :price)
+  end
+
+  def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"] # PAY.JPテスト秘密鍵
     Payjp::Charge.create(
       amount: @item.price,
       card: usaer_donation_params[:token],
       currency:'jpy'                 
     )
-  end
-
-  def donnation_params
-    params.require(:user_donation).permit(:name, :name_reading, :nickname, :postal_code, :prefecture_id, :city, :addresses, :building_name, :price)
   end
 end
 
